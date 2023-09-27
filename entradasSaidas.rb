a54596242a381cce6c4707a1bee05d76573bfe8e
unknown <josepsneto@gmail.com>
2023-09-27 08:49:04 -0300

    First commit

---
+++ entradasSaidas.rb
@@ -0,0 +1,14 @@
+# 1) Saída de dados na tela: Usuário digite se nome
+
+print "Digite seu nome: "
+
+# 2) Programa deve permitir que o usuario preencha o que foi pedido
+nome = gets.chomp 
+
+print "Digite seu sobrenome: "
+
+sobrenome = gets.chomp
+
+# 3) Saída final
+
+puts "Oi, #{nome} #{sobrenome}!" 
