19de9f190a06f79c07c4255ff2e5f1c05fd9c8af
zeneto <josepsneto@gmail.com>
2023-09-27 08:57:23 -0300

    Adiciona o primeiro desafio de código, 'nome completo mais idade'

---
+++ nomecompleto.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+idade = gets.chomp
+
+puts "Olá, #{nome} #{sobrenome}, sua idade é #{idade} anos."
